ca18c0d69fd0a4634d36214e7572335e9e9ff43b
ayazhafiz <ayaz.hafiz.1@gmail.com>
2019-12-24 17:59:26 -0600

    d: add crate documentation

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-# xorfilter
+# xorf
 
 [![Build Status](https://travis-ci.com/ayazhafiz/xorf.svg?branch=master)](https://travis-ci.com/ayazhafiz/xorf)
 
 This repository hosts a Rust library implementing
-[xor filters](https://arxiv.org/abs/1912.08258). Daniel Lemire's
-[go implementation](https://github.com/FastFilter/xorfilter) provides a useful
-summary of their benefits and listing of other xor filter libraries.
+[xor filters](https://arxiv.org/abs/1912.08258) -- data structures for fast
+approximation of set membership using little memory. Probabilistic filters like
+xor filters are useful for quickly estimating of the existence of an entity to
+avoid using an expensive resource. For example, they can be used to
+[reduce disk writes](https://en.wikipedia.org/wiki/Bloom_filter#Cache_filtering)
+in a cache or
+[identify malicious URLs](https://en.wikipedia.org/wiki/Bloom_filter#Examples).
 
-Xor filters operate on 64-bit unsigned keys. Details on usage of the filters can
-be found in the library documentation.
+Xor filters are faster and smaller than Bloom and Cuckoo filters. Xor filters
+incur a relative time penalty in construction, but are very fast in lookups; the
+expectation is that construction of a filter is amortized after many queries.
+Daniel Lemire's [go implementation](https://github.com/FastFilter/xorfilter)
+provides a useful summary of xor filters' benefits and listing of other xor
+filter libraries.
+
+Xor filters operate on sets ofs 64-bit unsigned integers. Information regarding
+usage of the filters can be found in the
+[library documentation](https://docs.rs/xorf).
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
+//! This library implements Xor Filters -- data structures for fast approximation of set
+//! membership using little memory. Probabilistic filters like xor filters are useful for
+//! quickly estimating of the existence of an entity to avoid using an expensive resource.
+//! For example, they can be used to [reduce disk writes] in a cache or [identify malicious URLs].
 //!
+//! Xor filters are faster and smaller than Bloom and Cuckoo filters.
+//! Xor filters incur a relative time penalty in construction, but are very fast in lookups; the
+//! expectation is that construction of a filter is amortized after many queries.
 //!
+//! Xor filters operate only on sets of 64-bit (unsigned) integers. This library does not provide
+//! methods for hashing arbitrary types to 64-bit integers. Xor filters are immutable,
+//! serializable, and guarantee no false negatives.
+//!
+//! Filters provided by this library are implemented as described in the paper
+//! [Xor Filters: Faster and Smaller Than Bloom and Cuckoo Filters] and in Daniel Lemire's [go] and
+//! [c] implementations. All are useful references on the theory behind xor filters. For
+//! performance statistics, refer to individual filters' documentation or the mentioned
+//! paper.
+//!
+//! [Xor Filters: Faster and Smaller Than Bloom and Cuckoo Filters]: https://arxiv.org/abs/1912.08258
+//! [reduce disk writes]: https://en.wikipedia.org/wiki/Bloom_filter#Cache_filtering
+//! [identify malicious URLs]: https://en.wikipedia.org/wiki/Bloom_filter#Examples
+//! [go]: https://github.com/FastFilter/xorfilter
+//! [c]: https://github.com/FastFilter/xor_singleheader
 
 #![no_std]
 #![cfg_attr(feature = "nightly", feature(allocator_internals), needs_allocator)]
